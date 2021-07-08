97b78b16179cd2c4316d8bf3c964117a457fa008
Ratan206thk <Ratan206thk@gmail.com>
2021-07-09 00:43:02 +0530

    'ini'

--- README.md
+++
@@ -1,2 +0,0 @@
-# php-mail-test
-testing
---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'plugins/PHPMailer/src/Exception.php';
+require 'plugins/PHPMailer/src/PHPMailer.php';
+require 'plugins/PHPMailer/src/SMTP.php';
+$mail = new PHPMailer(true);
+
+try {
+    //Recipients
+    $mail->setFrom('ratan206thk@gmail.com', 'Mailer');
+    $mail->addAddress('aaryathakur315@gmail.com', 'Joe User');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
