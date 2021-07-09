36b07ae20e6c19eec9d6bf810e22991edabfd896
Ratan206thk <Ratan206thk@gmail.com>
2021-07-09 13:26:09 +0530

    bla

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+require 'plugins/PHPMailer/src/Exception.php';
+require 'plugins/PHPMailer/src/PHPMailer.php';
+require 'plugins/PHPMailer/src/SMTP.php';
 $mail = new PHPMailer(true);
 
 try {
